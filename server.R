c12f739a4f6c8d29b9507dce212de8c5c4b1e234
Joseph Ilagan <joben.ilagan@gmail.com>
2015-11-08 23:45:48 +0800

    Added gh-pages

---
+++ server.R
@@ -0,0 +1,19 @@
+library(shiny)
+library(UsingR)
+data(galton)
+diabetesRisk <- function(glucose) glucose / 200
+shinyServer(
+        function(input, output) {
+                output$inputValue <- renderPrint({input$glucose})
+                output$prediction <- renderPrint({diabetesRisk(input$glucose)})
+                output$newHist <- renderPlot({
+                        hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
+                        mu <- input$mu
+                        lines(c(mu, mu), c(0, 200),col="red",lwd=5)
+                        mse <- mean((galton$child - mu)^2)
+                        text(63, 150, paste("mu = ", mu))
+                        text(63, 140, paste("MSE = ", round(mse, 2)))
+                })
+        
+        }
+)
